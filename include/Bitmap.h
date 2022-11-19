7e6085ecb4eea38cebf29fc1546509020d3439b2
MSFTKevin <keorba@microsoft.com>
2022-11-19 13:27:56 -0800

    Initial commit

---
+++ include/Bitmap.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdint.h>
+
+extern const uint16_t SeeedstudioBitmapWidth;
+extern const uint16_t SeeedstudioBitmapHeight;
+extern const unsigned short SeeedstudioBitmap[];
